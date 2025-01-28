a78a4eb6d86b94219cb1088ebcd3e884c0c33ae5
Joachim Desroches <jdesroches@kleis.ch>
2025-01-28 07:42:47 +0100

    Helix configuration

---
+++ helix.nix
@@ -0,0 +1,44 @@
+{ ... }:
+
+{
+  programs.helix = {
+    enable = true;
+    defaultEditor = true;
+    settings = {
+      theme = "ayu_evolve";
+      editor = {
+        bufferline = "multiple";
+        color-modes = true;
+        mouse = false;
+        statusline = {
+          left = [
+            "mode"
+            "spinner"
+            "version-control"
+            "read-only-indicator"
+            "file-modification-indicator"
+          ];
+          center = [ "file-name" ];
+          right = [
+            "diagnostics"
+            "workspace-diagnostics"
+            "file-encoding"
+            "position"
+            "position-percentage"
+            "total-line-numbers"
+          ];
+        };
+        soft-wrap.enable = true;
+        true-color = true;
+      };
+      keys.insert = {
+        C-n = "completion";
+      };
+      keys.normal = {
+        G = "goto_file_end";
+        space.w = ":w";
+        space.q = ":q";
+      };
+    };
+  };
+}
