c43eef603198fd21e992e577d377830d853d96b4
Joachim Desroches <jdesroches@kleis.ch>
2025-02-03 15:34:10 +0100

    Add shell abbreviations for jj to fish

--- programs/jujutsu/default.nix
+++ programs/jujutsu/default.nix
@@ -4,22 +4,39 @@
   home.packages = with pkgs; [
     difftastic
   ];
-  programs.jujutsu = {
-    enable = true;
-    settings = {
-      user = {
-        name = "Joachim Desroches";
-        email = "jdesroches@kleis.ch";
-      };
-      ui = {
-        default-command = "status";
-        diff = {
-          tool = ["difft" "--color=always" "$left" "$right"];
+  programs = {
+    jujutsu = {
+      enable = true;
+      settings = {
+        user = {
+          name = "Joachim Desroches";
+          email = "jdesroches@kleis.ch";
         };
-        pager = "less -FRX";
+        ui = {
+          default-command = "status";
+          diff = {
+            tool = [
+              "difft"
+              "--color=always"
+              "$left"
+              "$right"
+            ];
+          };
+          pager = "less -FRX";
+        };
+        revsets = {
+          log = "@ | ancestors(remote_bookmarks().., 2) | trunk()";
+        };
       };
-      revsets = {
-        log =  "@ | ancestors(remote_bookmarks().., 2) | trunk()";
+    };
+    fish = {
+      shellAbbrs = {
+        jjb = "jj bookmark";
+        jjd = "jj diff";
+        jje = "jj edit";
+        jjl = "jj log";
+        jjla = "jj log -r 'trunk()::(@ | tracked_remote_bookmarks())'";
+        jjw = "jj desc -m ";
       };
     };
   };
