519c3ae4f89fc4c2fa52f27e357d132870040f6e
Joachim Desroches <jodesroches@pictet.ch>
2025-04-16 15:19:10 +0200

    ssh: import configuration from mournblade.

---
+++ programs/ssh/default.nix
@@ -0,0 +1,17 @@
+_:
+
+{
+  programs.ssh = {
+    enable = true;
+    matchBlocks = {
+      "synokleis" = {
+        hostname = "192.168.1.6";
+        user = "jde";
+      };
+      "kleisap" = {
+        hostname = "192.168.1.2";
+        user = "root";
+      };
+    };
+  };
+}
