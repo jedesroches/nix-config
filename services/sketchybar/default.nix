1d2848d33a8934e7069f3cf13604d096aae0de02
Joachim Desroches <jodesroches@pictet.ch>
2025-10-06 08:53:41 +0200

    sketchybar: update wifi script to workaround macos

--- services/sketchybar/default.nix
+++ services/sketchybar/default.nix
@@ -9,54 +9,64 @@
         name = "setBarDate";
         runtimeInputs = [ sketchybar ];
         text = ''
-        sketchybar --set clock label="$(date "+%F %a %H:%M")"
+          sketchybar --set clock label="$(date "+%F %a %H:%M")"
         '';
       })
       (writeShellApplication {
         name = "setBarBattery";
         runtimeInputs = [ sketchybar ];
         text = ''
-        sketchybar --set battery \
-          label="$(pmset -g batt | grep -Eo "[0-9]+%" | cut -d% -f1)"
+          sketchybar --set battery \
+            label="$(pmset -g batt | grep -Eo "[0-9]+%" | cut -d% -f1)"
         '';
       })
       (writeShellApplication {
         name = "setBarWifi";
         runtimeInputs = [ sketchybar ];
         text = ''
-        sketchybar --set wifi \
-          label="$(ipconfig getsummary en0 | awk '/ SSID/{ print $3 }')"
+          wifi_name=$(networksetup -listpreferredwirelessnetworks en0 | awk 'NR == 2 { print $1 }')
+          en0_status=$(ifconfig en0 | awk '/status/ { print $2 }')
+
+          if [ "$en0_status" = "active" ];
+          then
+            wifi_label="<$wifi_name>"
+          else
+            wifi_label="<off>"
+          fi
+
+          sketchybar --set wifi \
+            label="$wifi_label"
         '';
       })
     ];
     config = ''
-    sketchybar                         \
-      --bar                            \
-        color=0xff000000               \
-        height=20                      \
-        position=top                   \
-        padding_left=10                \
-        padding_right=10               \
-      --default label.color=0xffcc6d00 \
-      --default icon.color=0xffcc6d00  \
-      --add item clock right           \
-      --set clock                      \
-        update_freq=10                 \
-        script=setBarDate              \
-        icon='|'                       \
-        label.padding_left=5           \
-        background.padding_left=5      \
-      --add item battery right         \
-      --set battery                    \
-        update_freq=30                 \
-        script=setBarBattery           \
-        icon='|'                       \
-        label.padding_left=5           \
-        background.padding_left=5      \
-      --add item wifi right            \
-      --set wifi                       \
-        update_freq=30                 \
-        script=setBarWifi               
+      sketchybar                         \
+        --bar                            \
+          color=0xff000000               \
+          height=20                      \
+          position=top                   \
+          padding_left=10                \
+          padding_right=10               \
+        --default label.color=0xffcc6d00 \
+        --default icon.color=0xffcc6d00  \
+        --add item clock right           \
+        --set clock                      \
+          update_freq=10                 \
+          script=setBarDate              \
+          icon='|'                       \
+          label.padding_left=5           \
+          background.padding_left=5      \
+        --add item battery right         \
+        --set battery                    \
+          update_freq=30                 \
+          script=setBarBattery           \
+          icon='|'                       \
+          label.padding_left=5           \
+          background.padding_left=5      \
+        --add item wifi right            \
+        --set wifi                       \
+          update_freq=30                 \
+          script=setBarWifi               
     '';
   };
 }
